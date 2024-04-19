b924b5589fa75b2606ce4835730e505bdaefff8a
Chris Kang <chris.v.kang@gmail.com>
2024-04-18 23:02:54 -0700

    chore: moved db query to handler

--- app/api/calendar-event/route.ts
+++ app/api/calendar-event/route.ts
@@ -1,23 +1,22 @@
 import { auth } from "@/auth";
-import { getCalendarEventByRange } from "@/data/calendar-event";
+import { getUserByEmail } from "@/data/user";
+import { prisma } from "@/lib/db";
 import { NextRequest } from "next/server";
 import status from "statuses";
 
 export async function GET(request: NextRequest) {
-  const session = await auth();
-
-  if (!session) {
-    return Response.json(
-      { message: "Not authorized" },
-      { status: status("unauthorized") }
-    );
-  }
-
   try {
+    const session = await auth();
+    if (!session || !session.user || !session.user.email) {
+      return Response.json(
+        { message: "Not authorized" },
+        { status: status("unauthorized") }
+      );
+    }
+
     const searchParams = request.nextUrl.searchParams;
     const start = searchParams.get("start");
     const end = searchParams.get("end");
-
     if (!start || !end) {
       return Response.json(
         { message: "Invalid query params" },
@@ -25,11 +24,25 @@ export async function GET(request: NextRequest) {
       );
     }
 
-    const response = await getCalendarEventByRange({
-      from: new Date(Number(start)),
-      to: new Date(Number(end)),
-    });
+    const user = await getUserByEmail(session.user.email);
+    if (!user) {
+      return Response.json(
+        { message: "User not found" },
+        { status: status("not found") }
+      );
+    }
 
+    const response = await prisma.calendarEvent.findMany({
+      where: {
+        userId: user.id,
+        start: {
+          gte: new Date(Number(start)),
+        },
+        end: {
+          lte: new Date(Number(end)),
+        },
+      },
+    });
     return Response.json(response);
   } catch (error) {
     return Response.json({ success: false, error });
