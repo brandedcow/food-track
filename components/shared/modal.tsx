82413eb96e2659c755a4fd1ae4f0a25cdd3eb8ab
Chris Kang <chris.v.kang@gmail.com>
2024-04-22 13:30:46 -0700

    feat: added background for modal

--- components/shared/modal.tsx
+++ components/shared/modal.tsx
@@ -9,7 +9,7 @@ export const Modal = ({
   children,
 }: PropsWithChildren<ModalProps>) => {
   return (
-    <div className="absolute h-full w-full flex items-center justify-center p-24 bg-transparent">
+    <div className="absolute h-full w-full flex items-center justify-center p-24 bg-black bg-opacity-50">
       {children}
     </div>
   );
