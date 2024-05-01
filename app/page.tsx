d7a94c990ffc261d0beebea147c79fe5146136a1
Chris Kang <chris.v.kang@gmail.com>
2024-05-01 09:36:20 -0700

    chore: redirect to login for base path

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import { LoginButton } from "@/components/auth/login-button";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-y-6">
-      <div className="text-xl font-bold">Food Track</div>
-      <LoginButton>Enter</LoginButton>
-    </main>
-  );
+  redirect("/auth/login");
+
+  return null;
+  // <main className="flex min-h-screen flex-col items-center justify-center gap-y-6">
+  //   <div className="text-xl font-bold">Food Track</div>
+  //   <LoginButton>Enter</LoginButton>
+  // </main>
 }
